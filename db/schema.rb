1dafcdb34762d8f59771d756a470256474f23950
clemence vert <clemence.legrand@yahoo.fr>
2023-03-23 15:51:31 +0100

    modification du form pour laisser uniquement email et passeword

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_095603) do
     t.bigint "monument_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "number_of_guests"
     t.index ["monument_id"], name: "index_bookings_on_monument_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
