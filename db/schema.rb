3521a7153e16072ea4a8ef09d060643a40dd6a05
Ndwali Mwenenge <azer8663@outlook.com>
2023-03-23 19:46:15 +0400

    Created and added the map to the index + Edited the seeds and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_143945) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_111330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -70,6 +70,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_143945) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_monuments_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,33 +14,33 @@ User.destroy_all
 puts "going to create monuments"
 user1 = User.create!(email: "user@gmail.com", password: "123456")
 user2 = User.create!(email: "user2@gmail.com", password: "123456")
-monument1 = Monument.create!(name: "Big Ben", address: "London SW1A 0AA, United Kingdom", price_per_night: 2500, capacity:200, description: "It's a big clock named Ben", rating: 4.2, theme: "Clock", city: "London", country: "England", user_id: user1.id)
-monument2 = Monument.create!(name: "Tour Eiffel", address: "Champ de Mars, 5 Av. Anatole France, 75007 Paris", price_per_night: 2800, capacity: 2000, description: "That big metal thingy in the middle of Paris", rating: 3.8, theme: "Antenna", city: "Paris", country: "France", user_id: user1.id)
-monument3 = Monument.create!(name: "Taj Mahal", address: "Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001, India", price_per_night: 8200, capacity: 5000, description: "Would Mumtaz Mahal and Shah Jahan approve of you partying in their last resting place? Probably not. But we won't tell if you don't.", rating: 4.9, theme: "Mausoleum", city: "Agra", country: "India", user_id: user1.id)
-monument4 = Monument.create!(name: "The Great Pyramid of Giza", address: "Al Haram, Nazlet El-Semman, Al Giza Desert, Giza Governorate 3512201, Egypt", price_per_night: 3700, capacity: 930, description: "Big Triangular Tomb of a guy named Khufu. We do not take responsibility if you get lost in there.", rating: 4.8, theme: "Tomb", city: "Giza", country: "Egypt", user_id: user2.id)
-monument5 = Monument.create!(name: "Château de Versailles", address: "Place d'Armes, 78000 Versailles", price_per_night: 8600, capacity: 5700, description: "Some guy named Louis used to party in there. He doesn't have all of his head now so he can't anymore, But you can!", rating: 3.1, theme: "Pompous Castle", city: "Versailles", country: "France", user_id: user2.id)
+monument1 = Monument.create!(name: "Big Ben", address: "London SW1A 0AA", price_per_night: 2500, capacity:200, description: "It's a big clock named Ben", rating: 4.2, theme: "Clock", city: "London", country: "United Kingdom", user_id: user1.id, latitude: 51.5007325, longitude: -0.1249943)
+monument2 = Monument.create!(name: "Tour Eiffel", address: "Champ de Mars, 5 Av. Anatole France, 75007 Paris", price_per_night: 2800, capacity: 2000, description: "That big metal thingy in the middle of Paris", rating: 3.8, theme: "Antenna", city: "Paris", country: "France", user_id: user1.id, latitude: 48.8583736, longitude: 2.2922926)
+monument3 = Monument.create!(name: "Taj Mahal", address: "Dharmapuri, Forest Colony, Tajganj, Agra, Uttar Pradesh 282001", price_per_night: 8200, capacity: 5000, description: "Would Mumtaz Mahal and Shah Jahan approve of you partying in their last resting place? Probably not. But we won't tell if you don't.", rating: 4.9, theme: "Mausoleum", city: "Agra", country: "India", user_id: user1.id, latitude: 27.1750855, longitude: 78.0414265)
+monument4 = Monument.create!(name: "The Great Pyramid of Giza", address: "Al Haram, Nazlet El-Semman, Al Giza Desert, Giza Governorate 3512201", price_per_night: 3700, capacity: 930, description: "Big Triangular Tomb of a guy named Khufu. We do not take responsibility if you get lost in there.", rating: 4.8, theme: "Tomb", city: "Giza", country: "Egypt", user_id: user2.id, latitude: 29.9792391, longitude: 31.1320132)
+monument5 = Monument.create!(name: "Château de Versailles", address: "Place d'Armes, 78000 Versailles", price_per_night: 8600, capacity: 5700, description: "Some guy named Louis used to party in there. He doesn't have all of his head now so he can't anymore, But you can!", rating: 3.1, theme: "Pompous Castle", city: "Versailles", country: "France", user_id: user2.id, latitude: 48.8049426, longitude: 2.1198078)
 # Monument.create!(name:, address:, price_per_night:, capacity:, description:, rating:, theme:, city:, country:, user_id: user1.id)
 # Monument.create!(name:, address:, price_per_night:, capacity:, description:, rating:, theme:, city:, country:, user_id: user1.id)
 # Monument.create!(name:, address:, price_per_night:, capacity:, description:, rating:, theme:, city:, country:, user_id: user1.id)
 # Monument.create!(name:, address:, price_per_night:, capacity:, description:, rating:, theme:, city:, country:, user_id: user1.id)
 
-file = URI.open("https://res.cloudinary.com/dfi1s3ujq/image/upload/v1679406348/cloudibigben_j2z2fm.jpg")
+file = URI.open("https://res.cloudinary.com/dfi1s3ujq/image/upload/v1679581650/bigben1920x1080_nu1tdg.jpg")
 monument1.photo.attach(io: file, filename: "bigben.png", content_type: "image/png")
 monument1.save
 
-file = URI.open("https://res.cloudinary.com/dfi1s3ujq/image/upload/v1679407277/cloudieiffel_iiztbv.jpg")
+file = URI.open("https://res.cloudinary.com/dfi1s3ujq/image/upload/v1679581660/eiffel-tower-hd-hf-1920x1080_ddy5dr.jpg")
 monument2.photo.attach(io: file, filename: "toureiffel.png", content_type: "image/png")
 monument2.save
 
-file = URI.open("https://res.cloudinary.com/dfi1s3ujq/image/upload/v1679407277/clouditajmahal_lcrx5g.jpg")
+file = URI.open("https://res.cloudinary.com/dfi1s3ujq/image/upload/v1679581688/tajmahal1920x1080_kxuzdj.jpg")
 monument3.photo.attach(io: file, filename: "tajmahal.png", content_type: "image/png")
 monument3.save
 
-file = URI.open("https://res.cloudinary.com/dfi1s3ujq/image/upload/v1679407277/cloudipyramide_emzub8.jpg")
+file = URI.open("https://res.cloudinary.com/dfi1s3ujq/image/upload/v1679581672/pyramid1920x1080_l2nx2g.webp")
 monument4.photo.attach(io: file, filename: "pyramid.png", content_type: "image/png")
 monument4.save
 
-file = URI.open("https://res.cloudinary.com/dfi1s3ujq/image/upload/v1679407277/cloudiversailles_frr6jd.jpg")
+file = URI.open("https://res.cloudinary.com/dfi1s3ujq/image/upload/v1679581689/versailles1920x1080_vt8v4n.jpg")
 monument5.photo.attach(io: file, filename: "versailles.png", content_type: "image/png")
 monument5.save
 
