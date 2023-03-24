05ce965deb8a845fa0aabb4f417c57e2980f22a8
clemence vert <clemence.legrand@yahoo.fr>
2023-03-24 10:16:14 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_03_23_141352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_23_141352) do
     t.bigint "monument_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.integer "number_of_guests"
     t.integer "days"
     t.index ["monument_id"], name: "index_bookings_on_monument_id"
